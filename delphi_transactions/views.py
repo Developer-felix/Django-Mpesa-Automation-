eb6b1013e08cd3d552c9c8b4e5a86e09c1e76e67
Developer-felix <onjombafelix1@gmail.com>
2021-09-13 15:58:21 +0300

    Create smart contract

--- delphi_transactions/views.py
+++ delphi_transactions/views.py
@@ -1,14 +1,59 @@
+import json
 from web3 import Web3
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Fill in your infura API key here
-infura_url = "https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY_GOES_HERE"
-web3 = Web3(Web3.HTTPProvider(infura_url))
+    infura_url = "https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY_GOES_HERE"
+    web3 = Web3(Web3.HTTPProvider(infura_url))
 
-print(web3.isConnected())
+def accounts(request):
+    
+    print(web3.isConnected())
+    print(web3.eth.blockNumber)
+    # Fill in your account here
+    balance = web3.eth.getBalance("YOUR_ACCOUNT_GOES_HERE")
+    print(web3.fromWei(balance, "ether"))
+    response = web3.fromWei(balance, "ether")
+    return Response({"Balance" : response})
 
-print(web3.eth.blockNumber)
+def read_smart_contracts(request):
+    # OMG Address
+    abi = json.loads('[{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]')
+    # OMG ABI
+    address = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07'
+    contract = web3.eth.contract(address=address, abi=abi)
+    totalSupply = contract.functions.totalSupply().call()
+    print(web3.fromWei(totalSupply, 'ether'))
+    total_suply = web3.fromWei(totalSupply, 'ether')
+    print(contract.functions.name().call())
+    function_name = contract.functions.name().call()
+    print(contract.functions.symbol().call())
+    function_call = contract.functions.symbol().call()
+    balance = contract.functions.balanceOf('0xd26114cd6EE289AccF82350c8d8487fedB8A0C07').call()
+    print(web3.fromWei(balance, 'ether'))
+    account_balance = web3.fromWei(balance, 'ether')
+    return Response({"Fumction name" : function_name,
+                     "function call " : function_call,
+                     "Total suply" : total_suply
+                     "Account balance" : account_balance
+    })
 
-# Fill in your account here
-balance = web3.eth.getBalance("YOUR_ACCOUNT_GOES_HERE")
-print(web3.fromWei(balance, "ether"))
-})
+def send_transactions(request):
+    ganache_url = "http://127.0.0.1:7545"
+    web3 = Web3(Web3.HTTPProvider(ganache_url))
+    account_1 = '' # Fill me in
+    account_2 = '' # Fill me in
+    private_key = '' # Fill me in
+    nonce = web3.eth.getTransactionCount(account_1)
+    tx = {
+    'nonce': nonce,
+    'to': account_2,
+    'value': web3.toWei(1, 'ether'),
+    'gas': 2000000,
+    'gasPrice': web3.toWei('50', 'gwei'),}
+    signed_tx = web3.eth.account.signTransaction(tx, private_key)
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    print(web3.toHex(tx_hash))
+    transactction_hash = web3.toHex(tx_hash) 
+    return Response({"Transaction hash" : transactction_hash})
